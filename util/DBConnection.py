7ff7b7c57ad7ec3a0675075f69ac77aae242bb55
Bheemalinga <kummarabheema@gmail.com>
2024-10-15 22:41:03 +0530

    Python,DB handshake setup

---
+++ util/DBConnection.py
@@ -0,0 +1,16 @@
+import pyodbc
+from util.Property import PropertyUtil
+
+class DBConnection:
+    connection = None
+
+    @staticmethod
+    def getConnection():
+        if DBConnection.connection is None:
+            try:
+                Credentials = PropertyUtil.getPropertyString()
+                DBConnection.connection = pyodbc.connect(Credentials)
+                print("\n\tPython and DB handshake Successful.")
+            except Exception as e:
+                print("Python - DB handshake failure : ",e)
+        return DBConnection.connection
---
+++ util/PropertyUtil.py
@@ -0,0 +1,9 @@
+class PropertyUtil:
+    @staticmethod # because 
+    def getPropertyString():
+        return (
+            'Driver = {SQL Server};'
+            'Server = BMA-DESKTOP-K4C\\SQLEXPRESS;'
+            'Database = Asset_Management;'
+            'Trusted_Connection = yes;'
+        ) 
